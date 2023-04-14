5c80a955f38fbf63a2887fa146c2895008a31a55
HaaniTharak <70282036+HaaniTharak@users.noreply.github.com>
2023-04-14 15:38:06 -0400

    test run3

--- Flask/main.py
+++ Flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+"""from flask import Flask
 from flask_restful import Api, Resource
 
 from website.main_API import *
@@ -49,18 +49,18 @@ def hello():
 if __name__ == "__main__":
     app.register_blueprint(bp)
     app.run(host = "localhost", port = 8080, debug=True)
+"""
+from flask import Flask, request
 
-# from flask import Flask, request
-
-# app = Flask(__name__)
-
-
+app = Flask(__name__)
 
 
 
 
-# if __name__ == "__main__":
-#     # Development only: run "python main.py" and open http://localhost:8080
-#     # When deploying to Cloud Run, a production-grade WSGI HTTP server,
-#     # such as Gunicorn, will serve the app.
-#     app.run(host="localhost", port=8080, debug=True)
+
+
+if __name__ == "__main__":
+    # Development only: run "python main.py" and open http://localhost:8080
+    # When deploying to Cloud Run, a production-grade WSGI HTTP server,
+    # such as Gunicorn, will serve the app.
+    app.run(host="localhost", port=8080, debug=True)
