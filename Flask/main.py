e030ae71375a7cc669b7dbd6c223a01f6a797a19
Jack Knox <knox36@purdue.edu>
2023-04-14 15:46:36 -0400

    Update main.py

--- Flask/main.py
+++ Flask/main.py
@@ -5,6 +5,12 @@ from website.frontend import bp
 
 app = create_app()
 
+@app.get("/")
+def hello():
+    """Return a friendly HTTP greeting."""
+    who = request.args.get("who", default="World")
+    return f"Hello {who}!\n"
+
 if __name__ == "__main__":
     app.register_blueprint(bp)
     app.run(host="localhost", port=8080, debug=True)
