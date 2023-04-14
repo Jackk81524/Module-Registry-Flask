add725f0a40d4ffdca21edd5e375892c13286dcf
Jack Knox <knox36@purdue.edu>
2023-04-14 15:00:04 -0400

    Update main.py

--- Flask/main.py
+++ Flask/main.py
@@ -1,11 +1,29 @@
-from website import create_app
-from flask import Flask, request
-from flask_restful import Api, Resource
-from website.frontend import bp
+# from website import create_app
+# from flask import Flask, request
+# from flask_restful import Api, Resource
+# from website.frontend import bp
 
-app = create_app()
+# app = create_app()
+
+
+# if __name__ == "__main__":
+#     app.register_blueprint(bp)
+#     app.run(host = "localhost", port = 8080, debug=True)
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def hello():
+    """Return a friendly HTTP greeting."""
+    who = request.args.get("who", default="World")
+    return f"Hello {who}!\n"
 
 
 if __name__ == "__main__":
-    app.register_blueprint(bp)
-    app.run(host = "localhost", port = 8080, debug=True)
+    # Development only: run "python main.py" and open http://localhost:8080
+    # When deploying to Cloud Run, a production-grade WSGI HTTP server,
+    # such as Gunicorn, will serve the app.
+    app.run(host="localhost", port=8080, debug=True)
